768592ba43bd812ebfae7f2cf97f301bdac596c3
absolute88 <absolute964688@gmail.com>
2025-06-11 10:37:58 +0900

    キー操作

--- src/shooting/Player.java
+++ src/shooting/Player.java
@@ -1,9 +1,29 @@
 package shooting;
+import java.awt.event.KeyEvent;
 
 public class Player extends Character{
 	public Player(double x,double y,double vx,double vy) {
 		super(x,y,vx,vy);
 	}
+	public void keyPressed(KeyEvent e) {
+		if(e.getKeyCode()==KeyEvent.VK_LEFT) {
+			vx=-5;
+		}
+		if(e.getKeyCode()==KeyEvent.VK_RIGHT) {
+			vx=+5;
+		}
+	}
+	
+	public void keyReleased(KeyEvent e) {
+		if(e.getKeyCode()==KeyEvent.VK_LEFT) {
+			vx=0;
+		}
+		if(e.getKeyCode()==KeyEvent.VK_RIGHT) {
+			vx=0;
+		}
+	}
+	
+	
 
 
 }
